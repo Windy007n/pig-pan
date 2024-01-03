c487ff3b880c5901686c3d3a13cc81c91b1d44af
min <timeset2544@gmail.com>
2024-01-03 14:12:28 +0700

    first commit

---
+++ v2/server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+
+app.set('views', path.join(__dirname, 'templates'))
+app.set('view engine', 'ejs')
+
+app.use("/static", express.static("static"))
+
+app.get("/", (req, res) => {
+
+    res.render('home', req.query)
+
+})
+
+app.listen(80, () => console.log("server listen on port 80"))
